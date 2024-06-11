c1e682148cd7bd2ac3a14727a42eab0a32620f03
Christian Hajek <chris@devseal.eu>
2024-06-11 17:18:25 +0200

    fixed some types; refactored code; add remove ingredient button

--- src/components/RecipeForm.tsx
+++ src/components/RecipeForm.tsx
@@ -1,7 +1,11 @@
 import { RecipeWithIngredients } from "@/utils/types";
 import { FormEvent, useState } from "react";
 
-export default function RecipeForm({ onSubmit }: {onSubmit: (data: RecipeWithIngredients) => void}) {
+export default function RecipeForm({
+  onSubmit,
+}: {
+  onSubmit: (data: RecipeWithIngredients) => void;
+}) {
   const [ingredientAmount, setIngredientAmount] = useState(1);
   const submitRecipe = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -14,18 +18,23 @@ export default function RecipeForm({ onSubmit }: {onSubmit: (data: RecipeWithIng
       ingredients: [...Array(ingredientAmount).keys()].map((index) => {
         return {
           name: formData.get(`name-${index}`),
-          amount: formData.get(`amount-${index}`),
+          amount: parseInt(formData.get(`amount-${index}`)!.toString()),
           units: formData.get(`units-${index}`),
         };
       }),
     });
-    event.currentTarget.reset()
+    event.currentTarget.reset();
   };
   return (
     <form onSubmit={submitRecipe}>
       <div className="flex gap-4 justify-center items-center mb-6">
         <label htmlFor="name">Name:</label>
-        <input id="name" name="name" placeholder="Banana Bread" required></input>
+        <input
+          id="name"
+          name="name"
+          placeholder="Banana Bread"
+          required
+        ></input>
       </div>
       <div className="flex gap-4 justify-center items-center mb-6">
         <label htmlFor="steps">Steps:</label>
@@ -39,37 +48,53 @@ export default function RecipeForm({ onSubmit }: {onSubmit: (data: RecipeWithIng
 
       {
         // @ts-ignore
-      [...Array(ingredientAmount).keys()].map((index) => {
-        return (
-          <fieldset
-            className="flex gap-4 justify-center items-center mb-6"
-            key={index}
-          >
-            <label htmlFor={`name-${index}`}>Ingredient Name:</label>
-            <input
-              id={`name-${index}`}
-              name={`name-${index}`}
-              placeholder="Bananas"
-            ></input>
-            <label htmlFor={`amount-${index}`}>Ingredient Amount:</label>
-            <input
-              id={`amount-${index}`}
-              type="number"
-              name={`amount-${index}`}
-              placeholder="6"
-            ></input>
-            <label htmlFor={`units-${index}`}>Ingredient Units:</label>
-            <input
-              id={`units-${index}`}
-              name={`units-${index}`}
-              placeholder="bunches"
-            ></input>
-          </fieldset>
-        );
-      })}
+        [...Array(ingredientAmount).keys()].map((index) => {
+          return (
+            <fieldset
+              className="flex gap-4 justify-center items-center mb-6"
+              key={index}
+            >
+              <label htmlFor={`name-${index}`}>Ingredient Name:</label>
+              <input
+                id={`name-${index}`}
+                name={`name-${index}`}
+                placeholder="Bananas"
+              ></input>
+              <label htmlFor={`amount-${index}`}>Ingredient Amount:</label>
+              <input
+                id={`amount-${index}`}
+                type="number"
+                name={`amount-${index}`}
+                placeholder="6"
+                required
+                defaultValue={0}
+              ></input>
+              <label htmlFor={`units-${index}`}>Ingredient Units:</label>
+              <input
+                id={`units-${index}`}
+                name={`units-${index}`}
+                placeholder="bunches"
+              ></input>
+            </fieldset>
+          );
+        })
+      }
       <button
         className="group underline rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-        onClick={() => setIngredientAmount((amount) => amount + 1)}
+        onClick={(event) => {
+          event.preventDefault();
+          setIngredientAmount((amount) => amount - 1);
+        }}
+        disabled={ingredientAmount <= 1}
+      >
+        Remove Ingredient
+      </button>
+      <button
+        className="group underline rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+        onClick={(event) => {
+          event.preventDefault();
+          setIngredientAmount((amount) => amount + 1);
+        }}
       >
         Add Ingredient
       </button>
